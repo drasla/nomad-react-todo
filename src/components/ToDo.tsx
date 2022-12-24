316ce521f7e1544766cb405efbd56aeff0f0aaf6
Tae.Garam <drasla@naver.com>
2022-12-24 18:07:10 +0900

    #6.12 Refactoring

---
+++ src/components/ToDo.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { IToDo } from "../atoms";
+
+function ToDo(text: IToDo) {
+    return (
+        <li>
+            <span>{text.text}</span>
+            <button>Doing</button>
+            <button>To Do</button>
+            <button>Done</button>
+        </li>
+    );
+}
+
+export default ToDo;
